cb48fd2e438dbf2051f36281d16a441936d56d8a
Stefan Pawlowski <Stefan.Pawlowski@Infinithings.de>
2023-05-12 02:36:33 +0200

    cleanup

--- cmd/list/pods.go
+++ cmd/list/pods.go
@@ -1,11 +1,8 @@
 package list
 
 import (
-	"context"
-	"fmt"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"kufast/clusterOperations"
 	"kufast/tools"
 	"os"
@@ -30,21 +27,15 @@ To gain further information see the kubectl get pod command.`,
 		//build table
 		t := table.NewWriter()
 		t.SetOutputMirror(os.Stdout)
-		t.AppendHeader(table.Row{"NAME", "NAMESPACE", "STATUS", "POD MESSAGE", "CONTAINER MESSAGE"})
+		t.AppendHeader(table.Row{"NAME", "NAMESPACE", "STATUS", "POD MESSAGE"})
 		for _, pod := range pods {
-			t.AppendRow(table.Row{pod.Name, pod.Namespace, pod.Status.Phase, pod.Status.Message, ""})
+			t.AppendRow(table.Row{pod.Name, pod.Namespace, pod.Status.Phase, pod.Status.Message})
 
 		}
 		s.Stop()
 		t.AppendSeparator()
 		t.Render()
 
-		clientset, _, _ := tools.GetUserClient(cmd)
-
-		events, _ := clientset.CoreV1().Events("testtenant3-w2").List(context.TODO(), metav1.ListOptions{})
-		for _, event := range events.Items {
-			fmt.Println(event)
-		}
 	},
 }
 
